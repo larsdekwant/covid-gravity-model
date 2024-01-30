6ba87259175e2a48a0c05b2e155352e67df552d8
larsdekwant <Larskwant@gmail.com>
2024-01-30 15:34:09 +0100

    create gemeenten2018.csv dataset

---
+++ MobilityModel/data_prep.py
@@ -0,0 +1,22 @@
+import configparser
+import pandas as pd
+import numpy as np
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+data_gem_inw = pd.read_csv(config['PATHS']['RAWDATA_GEM_INW'], delimiter=';', encoding='latin-1')
+data_gem_alfa = pd.read_csv(config['PATHS']['RAWDATA_GEM_ALFA'], delimiter=';', encoding='latin-1')
+
+data_gem_inw.dropna(subset='TotaleBevolking_1', inplace=True)
+
+new_data = pd.merge(data_gem_inw, data_gem_alfa[['GemeentecodeGM','Gemeentenaam']], on='GemeentecodeGM')
+
+new_data.to_csv('../Data/Gem_inwoners_totaal_2018.csv', sep=';', encoding='utf-8')
+
+
+
+
+print("waaa")
+
+
