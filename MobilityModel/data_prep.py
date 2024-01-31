90fc6854da0247421cf255b2727a4f2c8eb1b35b
larsdekwant <Larskwant@gmail.com>
2024-01-31 21:31:53 +0100

    fix demgraphic data prep

--- MobilityModel/data_prep.py
+++ MobilityModel/data_prep.py
@@ -11,12 +11,7 @@ data_gem_alfa = pd.read_csv(config['PATHS']['RAWDATA_GEM_ALFA'], delimiter=';', 
 data_gem_inw.dropna(subset='TotaleBevolking_1', inplace=True)
 
 new_data = pd.merge(data_gem_inw, data_gem_alfa[['GemeentecodeGM','Gemeentenaam']], on='GemeentecodeGM')
+new_data['GemeentecodeGM'] = new_data['GemeentecodeGM'].apply(lambda gmId: int(gmId.lstrip('GM')))
+new_data.rename(columns={"GemeentecodeGM": "Gemeentecode"}, inplace=True)
 
 new_data.to_csv('../Data/Gem_inwoners_totaal_2018.csv', sep=';', encoding='utf-8')
-
-
-
-
-print("waaa")
-
-
