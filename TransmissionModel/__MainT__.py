b091ee0b536b3f45296991b74784ffa12d4735a6
Lars de Kwant <l.c.dekwant@students.uu.nl>
2024-02-27 17:01:14 +0100

    add pienter data

--- TransmissionModel/__MainT__.py
+++ TransmissionModel/__MainT__.py
@@ -35,7 +35,7 @@ import scipy.sparse
 
 for interv in ['none']:
     for run in [0]:
-        for seed in range(0, 1):
+        for seed in range(5, 6):
             params_input = {'savename': 'High',
                             'intervention': interv,
                             'Ndays': 120*24,
