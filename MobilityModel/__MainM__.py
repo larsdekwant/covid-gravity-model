4826b257647a3c00245ee65166e183c7cfc54554
Matthijs_standard <m.cr@live.nl>
2024-07-01 14:58:46 +0200

    adding data. produced 1 figure. Not correct result yet.

--- MobilityModel/__MainM__.py
+++ MobilityModel/__MainM__.py
@@ -26,7 +26,7 @@ ClassM = ModelM(params_input)
 ClassM.read_data()
 print(ClassM.UniLocs)
 ClassM.mobility_matrix()
-for mc in [0]:
+for mc in [0,1,2,3,4,5,6,7,8,9]:
     ClassM.create_people_DF()
     ClassM.position_people()
     ClassM.count_people()
